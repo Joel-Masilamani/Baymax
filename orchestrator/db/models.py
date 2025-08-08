27f7b4557be767c5a694ef35e6b641b4da4e4cfb
Joel-Masilamani <dasspoo89389@gmail.com>
2025-08-08 11:22:13 +0530

    feat (db) : Added more tables for medicine manager

--- orchestrator/db/models.py
+++ orchestrator/db/models.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, Integer, String, Date, DateTime, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, DateTime, Boolean, ForeignKey , Enum
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import ARRAY
 from .database import Base
 from sqlalchemy import JSON
 from datetime import datetime, timezone
-
+import enum
 
 
 class User(Base):
@@ -30,7 +30,7 @@ class MedicalRecord(Base):
     start_date = Column(Date, nullable=False)
     end_date = Column(Date, nullable=False)
     created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
-
+    doses = relationship("DoseStatus", back_populates="record", cascade="all, delete-orphan")
     user = relationship("User", back_populates="records")
 
 class MedicationSchedule(Base):
@@ -47,3 +47,19 @@ class MedicationSchedule(Base):
     reminder_enabled = Column(Boolean, default=True)
 
     user = relationship("User", back_populates="medications")
+
+class DoseStatusEnum(str, enum.Enum):
+    pending = "pending"
+    taken = "taken"
+    missed = "missed"
+
+class DoseStatus(Base):
+    __tablename__ = "dose_statuses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    record_id = Column(Integer, ForeignKey("medical_records.id"), nullable=False)
+    dose_time = Column(DateTime, nullable=False)
+    status = Column(Enum(DoseStatusEnum), default=DoseStatusEnum.pending, nullable=False)
+    marked_at = Column(DateTime, nullable=True)
+
+    record = relationship("MedicalRecord", back_populates="doses")
